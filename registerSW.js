1d4af41ef4dc1a9ba874fc8a4a9182e55734b7f5
User <user@example.com>
2025-09-19 00:26:40 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mis-finanzas/sw.js', { scope: '/mis-finanzas/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
